8f142450ff9f8f75a8b3ac9f66eddd76c74152c5
Vladimir Rezvanov <ro6cket@gmail.com>
2026-04-13 20:58:05 +0700

    Написал загрузчик MNIST из ubyte.

---
+++ app/src/main/java/com/example/tsumaps/core/DigitRecognizer/loadMnist.kt
@@ -0,0 +1,36 @@
+package com.example.tsumaps.core.DigitRecognizer
+
+import android.content.Context
+import java.io.DataInputStream
+
+private fun loadMnistUbyte(maxImages: Int, context: Context): List<Pair<List<List<Int>>, Int>>{
+    val dataset = mutableListOf<Pair<List<List<Int>>, Int>>()
+    val labelsStream = DataInputStream(context.assets.open("train-labels.inx1-ubyte"))
+    val imagesStream = DataInputStream(context.assets.open("train-labels.inx3-ubyte"))
+
+    val magicLabels = labelsStream.readInt()
+    val numberLabels = labelsStream.readInt()
+
+    val magicImages = imagesStream.readInt()
+    val numberImages = imagesStream.readInt()
+    val rows = imagesStream.readInt()
+    val columns = imagesStream.readInt()
+
+    val countToRead = if (maxImages < numberImages) maxImages else numberImages
+    for (i in 0 until countToRead){
+        val label = labelsStream.readUnsignedByte()
+        val image = mutableListOf<List<Int>>()
+        for (row in 0 until rows){
+            val rowPixels = mutableListOf<Int>()
+            for (column in 0 until columns){
+                rowPixels.add(imagesStream.readUnsignedByte())
+            }
+            image.add(rowPixels)
+        }
+        dataset.add(Pair(image, label))
+    }
+    labelsStream.close()
+    imagesStream.close()
+
+    return dataset
+}
